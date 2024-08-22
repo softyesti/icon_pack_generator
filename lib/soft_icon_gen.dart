12d07bef033b135e0ef36374a7ed935b43a17819
João Sereia <joao.lo.sereia@gmail.com>
2024-08-22 18:10:58 -0300

    refactor: rearrange files

--- lib/soft_icon_gen.dart
+++ lib/soft_icon_gen.dart
@@ -1,8 +1,2 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/soft_icon_gen_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+/// An icon font generator for Flutter applications.
+library soft_icon_gen;
--- test/soft_icon_gen_test.dart
+++ test/soft_icon_gen_test.dart
@@ -1,16 +1,9 @@
-import 'package:soft_icon_gen/soft_icon_gen.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+    setUp(() {});
 
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    test('First Test', () {});
   });
 }
