d3a173ecdea4150a0e7e32d2d7ebf1bb46aedb6e
João Sereia <joao.lo.sereia@gmail.com>
2024-08-22 17:42:08 -0300

    initial commit

---
+++ example/soft_icon_gen_example.dart
@@ -0,0 +1,6 @@
+import 'package:soft_icon_gen/soft_icon_gen.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/soft_icon_gen_test.dart
@@ -0,0 +1,16 @@
+import 'package:soft_icon_gen/soft_icon_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
