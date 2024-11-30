7f55fbe34ec7cc0badce045fbadfd2082504f78d
João Sereia <joao.lo.sereia@gmail.com>
2024-11-30 20:30:17 -0300

    feat(lib/icon_pack_generator): add method to generate and create icon font file

--- lib/icon_pack_generator.dart
+++ lib/icon_pack_generator.dart
@@ -1,8 +1,10 @@
 /// An icon font generator for Flutter applications.
 library icon_pack_generator;
 
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:icon_pack_generator/entities/icon_entity.dart';
 import 'package:path/path.dart' as p;
 import 'package:recase/recase.dart';
 
@@ -34,5 +36,49 @@ class IconPackGenerator {
     }
 
     await Future.wait(queue);
+  }
+
+  Future<List<IconEntity>> genFont() async {
+    final name = className.snakeCase;
+    final source = p.absolute(iconSource.path);
+    final output = p.absolute(fontTarget.path);
+
+    final process = await Process.start(
+      'fantasticon',
+      runInShell: true,
+      [
+        source,
+        '--name',
+        name,
+        '--output',
+        output,
+        '--asset-types',
+        'json',
+        '--font-types',
+        'ttf',
+      ],
+    );
+
+    await stdout.addStream(
+      process.stdout.map((bytes) {
+        var message = utf8.decode(bytes);
+        if (message == '\x1b[32mDone\x1b[39m\n') {
+          message = '\x1b[32mSuccess generated font\x1b[39m\n';
+        }
+
+        return utf8.encode(message);
+      }),
+    );
+
+    final dataFile = File(p.setExtension(p.join(output, name), '.json'));
+    final dataString = await dataFile.readAsString();
+    final dataMap = jsonDecode(dataString) as Map<String, dynamic>;
+
+    final data = dataMap.entries.map((entry) {
+      return IconEntity(code: entry.value as int, name: entry.key);
+    }).toList();
+
+    await dataFile.delete();
+    return data;
   }
 }
