a8e943d280b29c86a0e53c891be92d3111462bc8
João Sereia <joao.lo.sereia@gmail.com>
2024-11-30 20:27:29 -0300

    feat(lib/icon_pack_generator): add method to rename files

--- lib/icon_pack_generator.dart
+++ lib/icon_pack_generator.dart
@@ -1,2 +1,38 @@
 /// An icon font generator for Flutter applications.
 library icon_pack_generator;
+
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+import 'package:recase/recase.dart';
+
+class IconPackGenerator {
+  const IconPackGenerator({
+    required this.className,
+    required this.packageName,
+    required this.iconSource,
+    required this.fontTarget,
+    required this.classTarget,
+  });
+
+  final String className;
+  final String packageName;
+  final Directory iconSource;
+  final Directory fontTarget;
+  final Directory classTarget;
+
+  Future<void> rename() async {
+    final files = await iconSource.list().toList();
+    final queue = <Future<FileSystemEntity>>[];
+
+    for (final file in files) {
+      final extension = p.extension(file.path);
+      final name = ReCase(p.basenameWithoutExtension(file.path)).snakeCase;
+      final newPath = p.join(iconSource.path, p.setExtension(name, extension));
+
+      queue.add(file.rename(newPath));
+    }
+
+    await Future.wait(queue);
+  }
+}
